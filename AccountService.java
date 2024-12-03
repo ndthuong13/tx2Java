4775ee0b07e74abd6b2b839a67868611900665e4
ThuongNguyen <117913699+ndthuong13@users.noreply.github.com>
2024-12-03 19:19:22 +0700

    Update

---
+++ AccountService.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public interface AccountService {
+    List<Account> getAllAccount();
+    void createNewAccount(Account account);
+    Account getAccountByUsername(String username);
+    void deleteAccount(String username);
+    void changeStatus(String username, Status status);
+}
